a29f1dfc63280554524f10a033d877890a576e51
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2023-04-19 19:44:50 -0300

    Add files via upload

---
+++ Calculadora/src/main/java/Calculadora.java
@@ -0,0 +1,22 @@
+class Calculadora {
+
+    public int somar(int n1, int n2) {
+        return n1 + n2;
+    }
+    
+    public int subtrair(int n1, int n2) {
+        return n1 - n2;
+    }
+    
+    public int multiplicar(int n1, int n2) {
+        return n1 * n2;
+    }
+    
+    public double dividir(double n1, double n2) {
+        return n1 / n2;
+    }
+    
+    public double potenciar(double n1, double n2) {
+        return Math.pow(n1, n2);
+    }
+}
